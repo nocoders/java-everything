388a31d3d7350ed3035386c421215c709d1a2d27
linmeng <linmeng0>
2023-02-26 11:25:57 +0800

    redis 分布式redisson重入代码修改 markdown文件修改

--- src/test/java/com/javaEverything/distributeLock/RedisLockRedisson.java
+++ src/test/java/com/javaEverything/distributeLock/RedisLockRedisson.java
@@ -5,20 +5,13 @@ import org.junit.runner.RunWith;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.script.DefaultRedisScript;
-import org.springframework.scripting.support.ResourceScriptSource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Collections;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 
 /**
- * setNx 加锁
- * lua脚本删除分布式锁
+ * Redisson 分布式锁实现
  *
  * @author linmeng
  * @date 2023/2/24 23:48
@@ -37,10 +30,15 @@ public class RedisLockRedisson {
 			lock.lock();
 			System.out.println("加锁成功");
 			TimeUnit.SECONDS.sleep(5);
-		}catch (Exception e){}
-		finally {
+			lock.lock();
+			System.out.println("加锁成功2");
+			TimeUnit.SECONDS.sleep(5);
+		} catch (Exception e) {
+		} finally {
 			lock.unlock();
 			System.out.println("解锁成功：");
+			lock.unlock();
+			System.out.println("解锁成功2：");
 
 		}
 	}
